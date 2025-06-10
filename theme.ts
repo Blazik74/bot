c71f2a483be096efadb01389bb7409f6214f60b0
Blazik74 <platonovleonid337@gmail.com>
2025-06-10 21:38:48 +0500

    Delete theme.ts

--- theme.ts
+++
@@ -1,9 +0,0 @@
-export type Theme = 'light' | 'dark';
-
-export function setTheme(theme: Theme) {
-  document.documentElement.setAttribute('data-theme', theme);
-}
-
-export function getTheme(): Theme {
-  return (document.documentElement.getAttribute('data-theme') as Theme) || 'dark';
-} 
