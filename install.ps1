731d4f9dc231fc6b129b0c5e74a3fcfb838268a1
Blazik74 <platonovleonid337@gmail.com>
2025-06-11 02:30:54 +0500

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,5 @@
+# Install dependencies
+npm install
+
+# Install dev dependencies
+npm install --save-dev @types/react @types/react-dom @types/styled-components @vitejs/plugin-react typescript vite 
