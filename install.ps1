4227dc0944579d8c4e2ee13928502e5eb9c5bb67
Blazik74 <platonovleonid337@gmail.com>
2025-06-11 03:57:25 +0500

    Delete install.ps1

--- install.ps1
+++
@@ -1,5 +0,0 @@
-# Install dependencies
-npm install
-
-# Install dev dependencies
-npm install --save-dev @types/react @types/react-dom @types/styled-components @vitejs/plugin-react typescript vite 
