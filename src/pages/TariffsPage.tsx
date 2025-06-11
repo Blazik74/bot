bcb679fa10a8ef8262647d5178db4d11e30af694
Blazik74 <platonovleonid337@gmail.com>
2025-06-11 18:55:15 +0500

    Update TariffsPage.tsx

--- src/pages/TariffsPage.tsx
+++ src/pages/TariffsPage.tsx
@@ -1,1 +1,12 @@
- 
+import React from 'react';
+
+const TariffsPage: React.FC = () => {
+  return (
+    <div>
+      <h2>Тарифы и оплата</h2>
+      {/* Здесь будет логика тарифов */}
+    </div>
+  );
+};
+
+export default TariffsPage; 
