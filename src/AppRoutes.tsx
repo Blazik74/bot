0a031838baae2fa799f82c0503462b343e07d46f
Blazik74 <platonovleonid337@gmail.com>
2025-06-12 19:21:50 +0500

    Add files via upload

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -7,19 +7,30 @@ import ProfilePage from './pages/ProfilePage';
 import TariffsPage from './pages/TariffsPage';
 
 const PageWrapper: React.FC<{ children: React.ReactNode }> = ({ children }) => (
-  <div className="page-content">{children}</div>
+  <motion.div
+    className="page-content"
+    initial={{ opacity: 0 }}
+    animate={{ opacity: 1 }}
+    exit={{ opacity: 0 }}
+    transition={{ duration: 0.3 }}
+  >
+    {children}
+  </motion.div>
 );
 
 const AppRoutes = () => {
   const location = useLocation();
+  
   return (
-    <Routes location={location} key={location.pathname}>
-      <Route path="/" element={<PageWrapper><AiCenterPage /></PageWrapper>} />
-      <Route path="/targetolog" element={<PageWrapper><TargetologPage /></PageWrapper>} />
-      <Route path="/profile" element={<PageWrapper><ProfilePage /></PageWrapper>} />
-      <Route path="/tariffs" element={<PageWrapper><TariffsPage /></PageWrapper>} />
-      <Route path="*" element={<Navigate to="/" />} />
-    </Routes>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<PageWrapper><AiCenterPage /></PageWrapper>} />
+        <Route path="/targetolog" element={<PageWrapper><TargetologPage /></PageWrapper>} />
+        <Route path="/profile" element={<PageWrapper><ProfilePage /></PageWrapper>} />
+        <Route path="/tariffs" element={<PageWrapper><TariffsPage /></PageWrapper>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </AnimatePresence>
   );
 };
 
