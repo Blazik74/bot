db6f62a58adc5fc210f06ca22f81c8aa39328efb
Blazik74 <platonovleonid337@gmail.com>
2025-06-13 19:14:49 +0500

    Update Targetolog.js

--- src/pages/Targetolog.js
+++ src/pages/Targetolog.js
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { useStore } from '../store';
+import useStore from '../store';
 
 export const Targetolog = () => {
   const theme = useStore((state) => state.theme);
