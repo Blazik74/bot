de335c2fc12ddfacbde041ced67f706acaa6f07b
Blazik74 <platonovleonid337@gmail.com>
2025-06-13 19:14:24 +0500

    Update TargetologPage.js

--- src/pages/TargetologPage/TargetologPage.js
+++ src/pages/TargetologPage/TargetologPage.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { motion } from 'framer-motion';
-import { useStore } from '../../store';
+import useStore from '../../store';
 import {
   Box,
   Typography,
