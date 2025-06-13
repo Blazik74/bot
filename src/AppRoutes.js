7e096ba0976ae1f8e630ad29b35d95e836ec5f3a
Blazik74 <platonovleonid337@gmail.com>
2025-06-14 03:10:00 +0500

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -6,6 +6,7 @@ import { Targetolog } from './pages/Targetolog';
 import Tariffs from './pages/Tariffs';
 import Profile from './pages/Profile';
 import { Notifications } from './components/Notifications';
+import FacebookConnect from './pages/FacebookConnect';
 
 const AppRoutes = () => {
   return (
@@ -17,6 +18,7 @@ const AppRoutes = () => {
           <Route path="/targetolog" element={<Targetolog />} />
           <Route path="/profile" element={<Profile />} />
           <Route path="/tariffs" element={<Tariffs />} />
+          <Route path="/facebook-connect" element={<FacebookConnect />} />
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </AnimatePresence>
