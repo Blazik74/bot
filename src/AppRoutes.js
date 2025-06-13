ffca75e4304f44a701446bb53d33379715c25930
Blazik74 <platonovleonid337@gmail.com>
2025-06-14 03:14:05 +0500

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
 import AICenter from './pages/AICenter';
-import { Targetolog } from './pages/Targetolog';
+import Targetolog from './pages/Targetolog';
 import Tariffs from './pages/Tariffs';
 import Profile from './pages/Profile';
 import { Notifications } from './components/Notifications';
