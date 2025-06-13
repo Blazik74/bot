2302c920adfa89950af445d8a00d90408815642f
Blazik74 <platonovleonid337@gmail.com>
2025-06-13 22:53:42 +0500

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -4,7 +4,7 @@ import { AnimatePresence } from 'framer-motion';
 import AICenter from './pages/AICenter';
 import { Targetolog } from './pages/Targetolog';
 import { Tariffs } from './pages/Tariffs';
-import { Profile } from './pages/Profile';
+import Profile from './pages/Profile';
 import BottomNavigation from './components/BottomNavigation';
 import { Notifications } from './components/Notifications';
 
