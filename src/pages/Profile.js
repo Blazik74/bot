bec6d60ebc26060a94612e80fb0216cd3249d044
Blazik74 <platonovleonid337@gmail.com>
2025-06-13 19:15:25 +0500

    Update Profile.js

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { useStore } from '../store';
+import useStore from '../store';
 
 export const Profile = () => {
   const theme = useStore((state) => state.theme);
