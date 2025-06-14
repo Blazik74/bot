aec07c9aaed7131734c8cad99a94ddefd9697a75
Blazik74 <platonovleonid337@gmail.com>
2025-06-14 17:18:06 +0500

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import { Notifications } from './components/Notifications';
+import AICenter from './pages/AICenter';
+import Targetolog from './pages/Targetolog';
+import Tariffs from './pages/Tariffs';
+import Profile from './pages/Profile';
+import FacebookConnect from './pages/FacebookConnect';
 
 const AppRoutes = () => {
-  return <Notifications />;
+  // Вставляй по одному компоненту внутрь return для проверки:
+  // return <Notifications />;
+  return <AICenter />;
+  // return <Targetolog />;
+  // return <Tariffs />;
+  // return <Profile />;
+  // return <FacebookConnect />;
+  return null; // Меняй на нужный компонент
 };
 
-export default AppRoutes; 
+export default AppRoutes;
