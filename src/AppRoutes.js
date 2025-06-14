b67cf10f16685466f8ba0d0fe319080be9bbcd6d
Blazik74 <platonovleonid337@gmail.com>
2025-06-15 03:46:05 +0500

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -12,11 +12,11 @@ const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<AICenter />} />
-      {/* <Route path="/targetolog" element={<Targetolog />} /> */}
-      {/* <Route path="/profile" element={<Profile />} /> */}
-      {/* <Route path="/tariffs" element={<Tariffs />} /> */}
-      {/*  <Route path="/facebook-connect" element={<FacebookConnect />} /> */}
-      // <Route path="*" element={<Navigate to="/" replace />} />
+      <Route path="/targetolog" element={<Targetolog />} />
+      <Route path="/profile" element={<Profile />} />
+      <Route path="/tariffs" element={<Tariffs />} />
+      <Route path="/facebook-connect" element={<FacebookConnect />} />
+      {/* <Route path="*" element={<Navigate to="/" replace />} /> */}
     </Routes>
   );
 };
