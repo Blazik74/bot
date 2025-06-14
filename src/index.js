1f32a5722b84a6d4b16f62c14a1055d6e9bb6e0c
Blazik74 <platonovleonid337@gmail.com>
2025-06-14 17:06:20 +0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from './contexts/ThemeContext';
-import AppRoutes from './AppRoutes';
+// import { BrowserRouter } from 'react-router-dom';
+// import { ThemeProvider } from './contexts/ThemeContext';
+// import AppRoutes from './AppRoutes';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <ThemeProvider>
-    <BrowserRouter>
-      <AppRoutes />
-    </BrowserRouter>
-  </ThemeProvider>
+  <div style={{color:'red',fontSize:40}}>Тестовый текст</div>
 );
 
 // If you want to start measuring performance in your app, pass a function
