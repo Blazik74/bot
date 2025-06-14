f18260099b84590ac8a3494f43f8200ee4a9c7a3
Blazik74 <platonovleonid337@gmail.com>
2025-06-14 07:27:06 +0500

    Update Main.js

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,1 +1,18 @@
-<Card active={selected === 'targetolog'}>ИИ Таргетолог</Card> 
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
+
+<Card active={selected === 'targetolog'}>ИИ Таргетолог</Card>
+
+// Для подсветки вкладки 'ИИ Центр' при открытой странице тарифов:
+const isTariffsPage = location.pathname === '/tariffs';
+<BottomNav active={selected === 'center' || isTariffsPage ? 'center' : selected} />
+
+// Для анимаций:
+<TransitionGroup>
+  <CSSTransition key={location.key} classNames="page" timeout={300}>
+    <Routes location={location}>
+      {/* ... */}
+    </Routes>
+  </CSSTransition>
+</TransitionGroup>
+
+// Добавить CSS для .page-enter, .page-enter-active, .page-exit, .page-exit-active 
