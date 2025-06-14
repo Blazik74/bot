50e9d0d09f4be3cb2608dd53630db62c033b38a2
Blazik74 <platonovleonid337@gmail.com>
2025-06-14 21:56:39 +0500

    Update Tariffs.js

--- src/pages/Tariffs.js
+++ src/pages/Tariffs.js
@@ -145,7 +145,7 @@ const Tariffs = () => {
   );
 };
 
-import Tariffs from './pages/Tariffs';
+export default Tariffs;
 
 // CSS для fadeInTariff
 // @keyframes fadeInTariff { from { opacity: 0; transform: translateY(30px);} to { opacity: 1; transform: none; } } 
