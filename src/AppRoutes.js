925488352feea666dda468be307196d534794d02
Blazik74 <platonovleonid337@gmail.com>
2025-06-14 17:14:19 +0500

    Update AppRoutes.js

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,26 +1,7 @@
 import React from 'react';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import AICenter from './pages/AICenter';
-import Targetolog from './pages/Targetolog';
-import Tariffs from './pages/Tariffs';
-import Profile from './pages/Profile';
-import { Notifications } from './components/Notifications';
-import FacebookConnect from './pages/FacebookConnect';
 
 const AppRoutes = () => {
-  return (
-    <>
-      <Notifications />
-      <Routes>
-        <Route path="/" element={<AICenter />} />
-        <Route path="/targetolog" element={<Targetolog />} />
-        <Route path="/profile" element={<Profile />} />
-        <Route path="/tariffs" element={<Tariffs />} />
-        <Route path="/facebook-connect" element={<FacebookConnect />} />
-        <Route path="*" element={<Navigate to="/" replace />} />
-      </Routes>
-    </>
-  );
+  return <div style={{color:'red',fontSize:40}}>Тестовый текст</div>;
 };
 
 export default AppRoutes; 
