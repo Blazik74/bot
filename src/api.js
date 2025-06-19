9f7aa91c99fdacf0f75fdc2053734ed5e6efc8d1
Blazik74 <platonovleonid337@gmail.com>
2025-06-19 21:38:34 +0500

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,10 @@
 import axios from "axios";
 
-const API_URL = process.env.http://127.0.0.1:8000/docs#/ || "http://localhost:8000";
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
 
 const api = axios.create({
   baseURL: API_URL,
-  withCredentials: true, // если нужны куки
+  withCredentials: true, // если нужны куки для авторизации
 });
 
 export default api;
