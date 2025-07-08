3d49e22d21454d7e55a956b95232a923b41ffacd
Blazik74 <platonovleonid337@gmail.com>
2025-07-08 21:15:13 +0500

    Update server.js

--- server.js
+++ server.js
@@ -42,7 +42,6 @@ app.use(session({
     resave: false,
     saveUninitialized: false,
     cookie: {
-        secure: process.env.NODE_ENV === 'production',
         sameSite: 'lax',
         maxAge: 24 * 60 * 60 * 1000
     }
